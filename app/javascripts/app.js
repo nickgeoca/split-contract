b261e5141ff0974a74237913de87f14a0f3fef1f
Nick Geoca <nickgeoca@gmail.com>
2016-10-26 16:26:05 -0500

    Initial commit

---
+++ app/javascripts/app.js
@@ -0,0 +1,67 @@
+var accounts;
+var account;
+
+function setStatus(message) {
+  var status = document.getElementById("status");
+  status.innerHTML = message;
+};
+
+function refreshBalance() {
+  var meta = MetaCoin.deployed();
+
+  meta.getBalance.call(account, {from: account}).then(function(value) {
+    var balance_element = document.getElementById("balance");
+    balance_element.innerHTML = value.valueOf();
+  }).catch(function(e) {
+    console.log(e);
+    setStatus("Error getting balance; see log.");
+  });
+};
+
+function sendCoin() {
+  var meta = MetaCoin.deployed();
+
+  var amount = parseInt(document.getElementById("amount").value);
+  var receiver = document.getElementById("receiver").value;
+
+  setStatus("Initiating transaction... (please wait)");
+
+  meta.sendCoin(receiver, amount, {from: account}).then(function() {
+    setStatus("Transaction complete!");
+    refreshBalance();
+  }).catch(function(e) {
+    console.log(e);
+    setStatus("Error sending coin; see log.");
+  });
+};
+
+window.onload = function() {
+  web3.eth.getAccounts(function(err, accs) {
+    if (err != null) {
+      alert("There was an error fetching your accounts.");
+      return;
+    }
+
+    if (accs.length == 0) {
+      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+      return;
+    }
+
+    accounts = accs;
+    account = accounts[0];
+
+    refreshBalance();
+  });
+}
+
+/*
+Q1: What is the address of metacoin? 
+there are 2 main external accounts: A and B
+* we can see the balance of the contract on the web page
+whenever someone sends ether to it, half of it goes to account A and the other half to account B
+* we can see the balances of A and B on the web page
+* we can send ether to it from the web page
+
+truffle console --network net42
+geth --datadir ~/.ethereum/net42 --networkid 42 console
+*/
---
+++ truffle.js
@@ -0,0 +1,21 @@
+module.exports = {
+  build: {
+    "index.html": "index.html",
+    "app.js": [
+      "javascripts/app.js"
+    ],
+    "app.css": [
+      "stylesheets/app.css"
+    ],
+    "images/": "images/"
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
+  }
+  networks: {
+    "net42": {
+      network_id: 42
+    }
+  }
+};
